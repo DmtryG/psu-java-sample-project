790ef3fd8a7b20214e9dba7e2bd8a6f89c89b4cd
Dmitriy Guryanov <guryanov.dmitriy@icloud.com>
2025-11-11 21:30:30 +0500

    Initial commit

---
+++ src/main/java/com/music/psujavasampleproject/AlbumManagerApplication.java
@@ -0,0 +1,13 @@
+package com.music.psujavasampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbumManagerApplication.class, args);
+    }
+
+}
